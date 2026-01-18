a53f4e2a96df57544d66a2bc280ce63f3328026c
gtapiero <giovanny.tapiero@pi.edu.co>
2026-01-18 16:26:41 -0500

    commit-app.routes.ts / .

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from '../login/login.component';
+import { FormComponent } from '../form/form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: LoginComponent },
+    { path: 'form', component: FormComponent }
+];  
